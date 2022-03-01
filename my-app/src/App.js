b0441826083cf16d77f9b27892c1baa1f9065a32
Doktan <thekonor@gmail.com>
2022-03-01 11:30:42 +0300

    ыыы
    ыыыыы

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,7 +21,7 @@ const MatrixInput = ({
                 num => {
                     const result = parseFloat(num)
                     return !isNaN(result) ? result : num
-            }))
+                }))
     }
 
     const validate = (value) => {
@@ -29,7 +29,7 @@ const MatrixInput = ({
         if (rows && currentRows.length !== rows) {
             return false
         }
-        if (columns && !currentRows.every(row => row.split(' ').length === columns)){
+        if (columns && !currentRows.every(row => row.split(' ').length === columns)) {
             return false
         }
         if (!currentRows.every(row => row.split(' ').every(num => num.match(/^(\d+[.,])?\d+$/)))) {
@@ -101,16 +101,16 @@ const App = () => {
     }
 
     const computeNet = (vector, matrix) => {
-        if (vector.length !== size || matrix.length !== size) return[]
-            const result = Array.from({ length: vector.length }).fill(0)
-            for (let i = 0; i < vector.length; i++) {
-                for (let j = 0; j < matrix.length; j++) {
-                    const vecElem = vector[i]
-                    const matrElem = matrix[j][i]
-                    if (typeof vecElem !== 'number' || typeof matrElem !== 'number') return []
-                    result[i] += vecElem * matrElem
-                }
+        if (vector.length !== size || matrix.length !== size) return []
+        const result = Array.from({ length: vector.length }).fill(0)
+        for (let i = 0; i < vector.length; i++) {
+            for (let j = 0; j < matrix.length; j++) {
+                const vecElem = vector[i]
+                const matrElem = matrix[j][i]
+                if (typeof vecElem !== 'number' || typeof matrElem !== 'number') return []
+                result[i] += vecElem * matrElem
             }
+        }
         return result
     }
 
@@ -133,11 +133,11 @@ const App = () => {
 
     const ResultColumn = ({ title, result }) => {
         return (
-            <div>
-            {title}
-            <div className={classes.net}>
-            {result.map(num => <div>{parseFloat(num.toFixed(4))}</div>)}
-            </div>
+            <div className='result'>
+                <div className='result__title'>{title}</div>
+                <div className='result__table'>
+                    {result.map(num => <div className='result__table__element'>{parseFloat(num.toFixed(4))}</div>)}
+                </div>
             </div>
         )
     }
@@ -145,7 +145,7 @@ const App = () => {
     return (
         <>
             <h2>Реализация матричной модели обработки информации в искусственных нейрон
-            ный сетях</h2>
+                ный сетях</h2>
             <p>Разработчики:</p>
             <ul>
                 <li>Мурашев А.С.</li>
@@ -153,56 +153,64 @@ const App = () => {
                 <li>Передерий В.А.</li>
                 <li>Капырин К.А.</li>
             </ul>
-        <hr />
-        <div className={classes.wrapper}>
-            <div>
-                Вектор
-                <MatrixInput
-                rows={5}
-                columns={1}
-                className={`${classes.matrix} ${classes.vector}`}
-                value={vector}
-                setValue={setVector}
-            />
+            <hr />
+            <div className='main__body'>
+                <div className='init'>
+                    <div>Вектор</div>
+                    <MatrixInput
+                        rows={5}
+                        columns={1}
+                        className={`${classes.matrix} ${classes.vector}`}
+                        value={vector}
+                        setValue={setVector}
+                    />
+                </div>
+                <div className='main__block'>
+                    <div className='main__init_block'>
+                        <div className='result__title'> Матрица W </div>
+                        <MatrixInput
+                            rows={5}
+                            columns={5}
+                            value={W}
+                            setValue={setW}
+                        />
+                    </div>
+                    <div className='main__result__block'>
+                        <ResultColumn title='NET1' result={Net1} />
+                        <ResultColumn title='OUT1' result={Out1} />
+                    </div>
+                </div>
+                <div className='main__block'>
+                    <div className='main__init_block'>
+                        <div className='result__title'> Матрица W </div>
+                        <MatrixInput
+                            rows={5}
+                            columns={5}
+                            value={V}
+                            setValue={setV}
+                        />
+                    </div>
+                    <div className='main__result__block'>
+                        <ResultColumn title='NET2' result={Net1} />
+                        <ResultColumn title='OUT2' result={Out1} />
+                    </div>
+                </div>
+                <div className={classes.offsetWrapper}>
+                    <span>Размер</span>
+                    <input
+                        type='number'
+                        className={classes.sizeInput}
+                        min={1}
+                        value={size}
+                        onChange={({ target }) => { setSize(target.value) }}
+                    />
+                    <button onClick={autoSetup}>Заполнить автоматически</button>
+                </div>
             </div>
-            <div>
-                Матрица W
-                <MatrixInput
-                rows={5}
-                columns={5}
-                className={classes.matrix}
-                value={W}
-                setValue={setW}
-            />
-            </div>
-            <ResultColumn title='NET1' result={Net1} />
-            <ResultColumn title='OUT1' result={Out1} />
-            <div>
-                Матрица V
-                <MatrixInput
-                rows={5}
-                columns={5}
-                className={classes.matrix}
-                value={V}
-                setValue={setV}
-            />
-            </div>
-            <ResultColumn title='NET2' result={Net2} />
-            <ResultColumn title='OUT2' result={Out2} />
-            </div>
-            <div className={classes.offsetWrapper}>
-                <span>Размер</span>
-                <input
-                    type='number'
-                    className={classes.sizeInput}
-                    min={1}
-                    value={size}
-                    onChange={({ target }) => { setSize(target.value) }}
-                />
-                <button onClick={autoSetup}>Заполнить автоматически</button>
-            </div>
+
         </>
     )
 }
+
 
 export default App
